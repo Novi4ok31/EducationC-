7beff89ed24851e391c3ecaf7c51572a953c71b7
Novi4ok <job_search31@mail.ru>
2022-08-20 14:46:34 +0300

    Добавил 2 задачу под номером 4

--- HomeWorks/HomeWork001/Program.cs
+++ HomeWorks/HomeWork001/Program.cs
@@ -1,5 +1,5 @@
 ﻿// HomeWork001
-
+/*
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // Вариант 1 рабочий
 Console.Write("Input a number1: ");
@@ -10,7 +10,7 @@ int max = number1;
 if (number2 > max)
     max = number2;
 Console.WriteLine("The maximum of two numbers is " + max);
-
+*/
 // Вариант 2 с ошибкой
 /*
 Console.Write("Input a number1: ");
@@ -24,3 +24,24 @@ if (number1 = number2)
     Console.WriteLine("The numbers are equal");
 Console.WriteLine("The maximum of two numbers is " + max);
 */
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{ 
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine("The maximum of three numbers is " + max);
+
