53e9a0952bb5f10dd522596104742f7a4aea33f1
Novi4ok <job_search31@mail.ru>
2022-08-20 22:59:04 +0300

    Сохранил 3 задачу

--- Seminars/Seminar2/Program.cs
+++ Seminars/Seminar2/Program.cs
@@ -1,7 +1,61 @@
-﻿
+﻿/*
 // Задача 1. Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
 
 int FindBiggestDigit(int number)
 {
-    
+    int ed = number %10;
+    int dec = number/10;
+    int max;
+
+    if (ed > dec)
+        max = ed;
+    else
+        max = dec;
+    return max;
 }
+
+int randomNumber = new Random().Next(10,100);
+// Генерирует случайное число. 
+int biggestDigit = FindBiggestDigit(randomNumber);
+Console.WriteLine ($"Biggest digit of {randomNumber} is {biggestDigit}");
+*/
+
+
+
+/*
+// Задача 2. Выводит случайное трехзначное число и удаляет вторую цифру 
+
+int CutNumber(int number)
+{
+    int ed = number % 10;
+    int sot = number / 100;
+    return sot * 10 + ed;
+}
+
+int randomNumber = new Random().Next(100,1000);
+int newNumber = CutNumber(randomNumber);
+Console.WriteLine ($"New version of {randomNumber} is {newNumber}");
+
+*/
+
+/*
+// Задача3. Напишите программу, которая будет принимать на вход два числа и выводить является ли второе число
+
+void IsDivisionable(int num1, int num2)
+{
+    int reminder = num2 % num1;
+    if( reminder == 0)
+        Console.WriteLine($"{num2} is divisionable of {num1}");
+    else
+        Console.WriteLine($"{num2} is not divisionable of {num1}. Reminder is {reminder}");
+}
+
+Console.Write("Input first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+IsDivisionable(n1, n2);
+*/
+
+// Задача 4
