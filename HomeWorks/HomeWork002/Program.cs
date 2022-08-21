c95f0fadf5f5e1399de3271136cd143c31424393
Novi4ok <job_search31@mail.ru>
2022-08-21 10:24:05 +0300

    Добавил задачу для поиска 2цифры трехзначного числа

--- HomeWorks/HomeWork002/Program.cs
+++ HomeWorks/HomeWork002/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// HomeWork002
+
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int CutDecNumber(int number)
+{
+    int dec = number /10;
+    dec = dec %10;
+    return dec;
+}
+Console.Write("Input a three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = CutDecNumber(number);
+Console.WriteLine($"Second digit of three-digit number is {result}");
+
