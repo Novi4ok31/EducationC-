4f4c6229a3131c9f8ad4e712d550768630dbb960
Novi4ok <job_search31@mail.ru>
2022-08-23 14:57:15 +0300

    Добавил вторую задачу-диапозон координат по заданной четверти.

--- Seminars/Seminar3/Program.cs
+++ Seminars/Seminar3/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Семинар №3
+// Задача 1 . Принимает на вход координаты точки отличные от нуля и выдает номер четверти, в которой находится эта точка
+/*
+double FindQuart(double x, double y)
+{
+    if(x>0 && y>0) return 1;
+    if(x<0 && y>0) return 2;
+    if(x<0 && y<0) return 3;
+    if(x>0 && y<0) return 4;
+
+    return 0;
+}
+
+Console.Write("Input first number: ");
+double xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+double yA = Convert.ToInt32(Console.ReadLine());
+
+double quartNum = FindQuart(xA,yA);
+
+if (quartNum == 0) 
+    Console.WriteLine("Point on the axes!");
+else
+    Console.WriteLine($"Point is located on {quartNum} quart.");
+*/
+
+// Программа, которая по заданному номеру четверти показывает диапозон возможных координат точек в этой четверти (x,y)
+
+void FindPoint(int quart)
+{
+    if(quart == 1) Console.WriteLine("Диапозон возможных координат (x>0,y>0)");
+    if(quart == 2) Console.WriteLine("Диапозон возможных координат (x<0,y>0)");
+    if(quart == 3) Console.WriteLine("Диапозон возможных координат (x<0,y<0)");
+    if(quart == 4) Console.WriteLine("Диапозон возможных координат (x>0,y<0)");
+}
+
+Console.Write("Input number of quart: ");
+int quart = Convert.ToInt32(Console.ReadLine());
+FindPoint(quart);
+
+   
