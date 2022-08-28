11ae2f9a139d68e0d094406df5b2044378d8dbe2
Novi4ok <job_search31@mail.ru>
2022-08-28 17:26:13 +0300

    Added first task

--- HomeWorks/HomeWork006/Program.cs
+++ HomeWorks/HomeWork006/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь.
+
+/*
+int [] CreateUserArray (int size)
+{
+    int [] newArray = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i+1} number of array: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int CountPositivNumber (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0) count ++;
+    }
+    return count;
+}
+
+Console.Write("Input size for array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateUserArray (size);
+ShowArray(myArray);
+int result = CountPositivNumber(myArray);
+Console.WriteLine($"The number of positive numbers in the array is {result}");
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+
+
+
