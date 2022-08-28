25a2a87a1406cfd494e10b0b28e774e37206084c
Novi4ok <job_search31@mail.ru>
2022-08-28 18:41:38 +0300

    Added task 2

--- HomeWorks/HomeWork006/Program.cs
+++ HomeWorks/HomeWork006/Program.cs
@@ -38,11 +38,28 @@ ShowArray(myArray);
 int result = CountPositivNumber(myArray);
 Console.WriteLine($"The number of positive numbers in the array is {result}");
 */
-
+/*
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
 // значения b1, k1, b2 и k2 задаются пользователем.
 
+void PointIntersection (double a1, double c1, double a2, double c2)
+{
+    double x = (c2 - c1) / (a1 - a2);
+    double y = a1 * x + c1;
+    if (a1==a2) Console.WriteLine("Straight parallel");
+    else Console.WriteLine($"The point of intersection of the lines has coordinates ({x},{y})");
+}
 
+Console.WriteLine ("Input the coefficient k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Input the coefficient b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Input the coefficient k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine ("Input the coefficient b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
 
+PointIntersection(k1,b1,k2,b2);
+*/
 
